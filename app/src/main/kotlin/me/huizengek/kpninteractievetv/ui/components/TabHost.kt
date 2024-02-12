b80443c0b7c3704cbd2f6e4b7a93331c440f2c42
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-02-12 16:10:52 +0100

    Optimize imports

--- app/src/main/kotlin/me/huizengek/kpninteractievetv/ui/components/TabHost.kt
+++ app/src/main/kotlin/me/huizengek/kpninteractievetv/ui/components/TabHost.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
--- kpn/src/main/kotlin/me/huizengek/kpnclient/KpnClient.kt
+++ kpn/src/main/kotlin/me/huizengek/kpnclient/KpnClient.kt
@@ -1,35 +1,20 @@
 package me.huizengek.kpnclient
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.accept
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
-import io.ktor.http.Cookie
 import io.ktor.http.contentType
-import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.decodeFromJsonElement
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
-import kotlinx.serialization.json.long
 import me.huizengek.kpnclient.requests.login
 import me.huizengek.kpnclient.util.CookieJar
-import me.huizengek.kpnclient.util.div
-import me.huizengek.kpnclient.util.plusAssign
-import me.huizengek.kpnclient.util.save
 import kotlin.time.Duration.Companion.minutes
 
 internal val json = Json {

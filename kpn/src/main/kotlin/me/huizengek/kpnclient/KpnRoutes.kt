c2aa4aeee7adae37a3444e0e53e816c09bfa51d0
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-06-21 11:24:26 +0200

    Update root route

--- kpn/src/main/kotlin/me/huizengek/kpnclient/KpnRoutes.kt
+++ kpn/src/main/kotlin/me/huizengek/kpnclient/KpnRoutes.kt
@@ -1,3 +1,3 @@
 package me.huizengek.kpnclient
 
-internal const val ROOT = "https://api.tv.prod.itvavs.prod.aws.kpn.com/101/1.2.0/A/nld/pctv/kpn"
+internal const val ROOT = "https://api-avs67.tv.prod.itvavs.prod.aws.kpn.com/101/1.2.0/A/nld/pctv/kpn"
